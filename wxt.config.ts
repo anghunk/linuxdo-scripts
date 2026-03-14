9fc12297411fa942fa18ba7a836180a6096e6717
imoyy <imoyyi@outlook.com>
2026-03-14 18:12:58 +0800

    feat: 限制插件仅作用于相关网页

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'wxt';
 import pkg from './package.json';
 
+const linuxDoMatches = ['https://linux.do/*', 'https://*.linux.do/*'];
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	extensionApi: 'chrome',
@@ -10,7 +12,8 @@ export default defineConfig({
 		version: pkg.version,
 		description: '为 linux.do 用户提供了一些增强功能。',
 		permissions: ['storage', 'sidePanel', 'tabs'],
-		host_permissions: ['http://*/*', 'https://*/*'],
+		host_permissions: linuxDoMatches,
+		optional_host_permissions: ['http://*/*', 'https://*/*'],
 		side_panel: {
 			default_path: 'sidepanel.html',
 		},
@@ -20,7 +23,7 @@ export default defineConfig({
 			if (wxt.config.command === 'serve') {
 				manifest.content_scripts ??= [];
 				manifest.content_scripts.push({
-					matches: ['https://linux.do/*'],
+					matches: linuxDoMatches,
 					js: ['content-scripts/content.js'],
 					css: ['content-scripts/content.css'],
 				});
